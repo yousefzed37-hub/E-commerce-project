4fbb485f815e35d054988b5b00164d8c36cc83f5
yousefzed37-hub <yousefzed37@gmail.com>
2026-08-09 08:15:42 +0300

    changes in order class

--- E-commerce project/Models/Order.cs
+++ E-commerce project/Models/Order.cs
@@ -18,6 +18,7 @@ namespace E_commerce_project.Models
         public string userName { get; set; } = string.Empty;
         public DateTime orderDate { get; set; } = DateTime.Now;
         public decimal totalAmount { get; set; }
+        public ICollection<OrderItem> orderItems { get; set; } = new List<OrderItem>();
         public OrderStatus orderStatus { get; set; } = OrderStatus.Pending;
     }
 }
